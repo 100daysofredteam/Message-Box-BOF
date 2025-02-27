3183e77ffe796647925fd5137bd338e6aafacb62
100daysofredteam <100daysofredteam@gmail.com>
2025-02-27 17:51:31 +0530

    Create message-box.c

---
+++ message-box.c
@@ -0,0 +1,21 @@
+#include <windows.h>
+#include <stdio.h>
+#include "beacon.h"
+
+WINUSERAPI int WINAPI USER32$MessageBoxA(HWND hWnd,LPCSTR lpText,LPCSTR lpCaption,UINT uType);
+WINBASEAPI DWORD WINAPI KERNEL32$GetLastError(void);
+
+int go(char * args, unsigned long len) {
+
+   BeaconPrintf(CALLBACK_OUTPUT, "Executing MessageBoxA on the target machine\n");
+   int result = USER32$MessageBoxA(NULL, "Thank you for joining 100 Days of Red Team." , "MessageBox BOF", MB_ICONINFORMATION | MB_OKCANCEL);
+   
+   if ( result ==0 ) {
+      BeaconPrintf(CALLBACK_ERROR, "Could not execute MessageBoxA. Encountered error: %d",KERNEL32$GetLastError());
+   }
+   else {
+      BeaconPrintf(CALLBACK_OUTPUT, "Successfully executed MessageBoxA on the target machine.\n");
+   }
+   
+   return 0;
+}
